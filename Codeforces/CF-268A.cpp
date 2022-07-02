37b4f83e0d21068979aef074e736d1906633132b
Mohammad-Jamil-Uddin <74015312+Jamil2914@users.noreply.github.com>
2022-07-02 16:19:31 +0600

    Add files via upload

--- Codeforces/CF-268A.cpp
+++ Codeforces/CF-268A.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    int a[n],b[n],i,j;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        cin>>b[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i]==b[j])
+            {
+                count++;
+            }
+        }
+    }
+    cout<<count<<endl;
 
+    return 0;
+}
